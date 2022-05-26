dbf52a82e26f1d1e8a0d00613cd6a804d8668f69
Quiza12 <Quiza12@live.com>
2022-05-26 21:35:22 +1000

    Testing Twitter post with Github Action.

--- test.js
+++ test.js
@@ -1,3 +1,79 @@
-console.log('Hello!');
+import graph from 'fbgraph';
+import Twitter from 'twitter';
+import fs from 'fs';
+import { parse } from 'csv-parse';
+import './env.js';
+
 const myArgs = process.argv.slice(2);
-console.log('myArgs: ', myArgs);
+const nnMap = new Map();
+var daysNanoNarrative = '';
+var caption = '';
+var mediaContainerId = '';
+var imageLink = 'https://raw.githubusercontent.com/Quiza12/NanoNarratives/master/images/';
+var uniqueImageName = '';
+var options = {
+    timeout:  30000,
+    pool:     { maxSockets:  Infinity },
+    headers:  { connection:  "keep-alive" }
+};
+var client = new Twitter({
+  consumer_key: myArgs[0],
+  consumer_secret: myArgs[1],
+  access_token_key: myArgs[2],
+  access_token_secret: myArgs[3]
+});
+
+function getDate() {
+  var today = new Date();
+  var yyyy = today.getFullYear();
+  var mm = today.getMonth() + 1; //months start at 0
+  var dd = today.getDate();
+
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+
+  caption = dd + '/' + mm + '/' + yyyy;
+  uniqueImageName = dd + mm + yyyy;
+  console.log("Posting for " + caption);
+  console.log("");
+}
+
+// Twitter ---------------------------->
+
+function findDaysNarrative() {
+  console.log("  Finding day's Nano Narrative...");
+  fs.createReadStream('./nn.csv')
+      .pipe(parse({delimiter: ','}))
+      .on('data', function(csvrow) {
+          nnMap.set(csvrow[0], csvrow[1]);
+          if (csvrow[0] == caption) {
+            daysNanoNarrative = csvrow[1];
+          }
+      })
+      .on('end',function() {
+        postTwitter();
+      });
+}
+
+function postTwitter() {
+  console.log("  Tweeting...");
+  client.post('statuses/update', { status: daysNanoNarrative },  function(error, tweet, response) {
+    if(error) throw error;
+    console.log("  Tweeted!");
+  });
+}
+
+function publishTwitter() {
+  console.log("Publishing on Twitter...");
+  findDaysNarrative();
+}
+
+function post() {
+  getDate();
+  publishInstagram();
+}
+
+console.log("");
+console.log("--Nano Narratives Poster--");
+console.log("");
+post();
